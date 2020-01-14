9e9be1f4903efa3ca8278c6828d34957175f3d74
Tom Rupnik <tomrupnik1@gmail.com>
2020-01-14 10:34:56 +0100

    zacetna vizualizacija

--- vizualizacija/moja_v.R
+++ vizualizacija/moja_v.R
@@ -1,5 +1,9 @@
-library(ggplot2)
+library(ggplot)
 
-dr_dolg <- ggplot(drzavni_dolg, aes(x=Leto, y= dolg_bdp)) + 
+dr_dolg <- ggplot(drzavni_dolg, aes(x=Leto, y= Dolg_bdp)) + 
      geom_line() +
      ggtitle("Državni dolg Slovenije") + xlab("Leto") + ylab("Dolg")
+
+bdp_graf <- ggplot(bdp, aes(x=Regija, y= Vrednost)) + geom_line() +
+  ggtitle("BDP Slovenije") + xlab("Regija") + ylab("Vrednost")
+  
